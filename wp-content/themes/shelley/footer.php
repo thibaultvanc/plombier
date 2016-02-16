8dc550603730a20dcdeb2532cd89eca3ccc1f1f5
Thibault <info@organitr.fr>
2016-02-16 23:47:53 +0100

    upload modif -4

--- wp-content/themes/shelley/footer.php
+++ wp-content/themes/shelley/footer.php
@@ -15,11 +15,11 @@
 		<div class="container">
 		<div class="col-md-12 text-center">
 			<!--<div class="span3"><a href="javascript: duoburner.newsletter.open();">Newsletter</a></div>-->
-			<div class="col-md-3"><a class="" href="/depannage-plomberie-idf/">Dépannage</a></div>
-			<div class="col-md-3"><a class="" href="/page/installation_ballon_chanudiere_idf">Installation</a></div>
-			<div class="col-md-3"><a class="" href="/contact">Contact</a></div>
+			<div class="col-md-4"><a class="" href="/depannage-plomberie-idf/">Dépannage</a></div>
+			<div class="col-md-4"><a class="" href="/page/installation_ballon_chanudiere_idf">Installation</a></div>
+			<div class="col-md-4"><a class="" href="/contact">Contact</a></div>
 		</div>
-		<div class="col-md-12">
+		<div class="col-md-12 hidden">
 			<div style="color: white;" class="span12">
 				<h3>Petit ou gros problème de plomberie ? N’attendez plus, et demandez un devis gratuit !</h3>
 				<p>Besoin d’un plombier? Notre entreprise vous propose ses services, que vous soyez un particulier ou un professionnel. Basée en plein centre de Paris, notre équipe de plombiers intervient également sur toutes la région parisienne et banlieues.</p>
@@ -27,13 +27,15 @@
 		</div>
 		<br>
 		<br>
-
-		<div class="col-md-12">
-			<!--<div class="span3"><a href="javascript: duoburner.newsletter.open();">Newsletter</a></div>-->
-			<div >1 PLOMBIER PRES DE CHEZ VOUS - 2 Rue Planchat - 75020 Paris - RCS 534 694 781</div>
 		<br>
-		<p>2 rue lucien jeannin 92250 La Garenne Colombes</p>
-		<p>2 rue Planchat 75020 Paris </p>
+		<br>
+
+		<div class="col-md-12 text-center">
+			<!--<div class="span3"><a href="javascript: duoburner.newsletter.open();">Newsletter</a></div>-->
+		<p style="margin: 0px" >1 PLOMBIER PRES DE CHEZ VOUS - 2 Rue Planchat - 75020 Paris - RCS 534 694 781</p>
+		<p style="margin: 0px">2 rue lucien jeannin 92250 La Garenne Colombes</p>
+		<p style="margin: 0px">2 rue Planchat 75020 Paris </p>
+			<p><a class="" href="/mentions-legales">Dépannage</a></p>
 		<br>
 		</div>
 		</div>
